52ee3f13a81f7d1c941d41761381b5cd195455cd
yasahaverland <yasahaverland@icloud.com>
2023-01-05 22:25:35 -0500

    render list of movies

--- netfreaks/screens/TabOneScreen.tsx
+++ netfreaks/screens/TabOneScreen.tsx
@@ -1,15 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, FlatList } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+
+import categories from '../assets/data/categories';
+
+const firstCategories = categories.items[0];
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
+      <Text style={styles.title}>Popular on Netfreaks</Text>
+        <FlatList 
+          data={firstCategories.movies}
+          renderItem={({item}) => (
+            <Image style={styles.image} source={{ uri: item.poster}} />
+          )}
+          horizontal
+        />
     </View>
   );
 }
@@ -17,9 +25,10 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#f2cd18d9'
+    justifyContent: 'flex-start',
+    backgroundColor: '#f2cd18d9',
+    padding: 20,
+
   },
   title: {
     fontSize: 20,
@@ -31,4 +40,11 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  image: {
+    width: 100,
+    height: 170,
+    resizeMode: 'cover',
+    borderRadius: 5,
+    margin: 5,
+  }
 });
