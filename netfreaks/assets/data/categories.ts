73daa7be4776d62b347fa524f00cddff15bebcfc
yasahaverland <yasahaverland@icloud.com>
2023-01-06 10:43:16 -0500

    home screen movies updated

--- netfreaks/assets/data/categories.ts
+++ netfreaks/assets/data/categories.ts
@@ -4,18 +4,21 @@ export default {
         title: 'Popular on Netflix',
         movies: [{
             id: 'movie1',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie1.jpg',
-        }, {
+            poster: 'https://resizing.flixster.com/UUK8BJZ5C4eQR4zb7j3I5Ux6ock=/206x305/v2/https://resizing.flixster.com/NtptCh0CBMVv3Vvv-niTT7E-5EA=/ems.cHJkLWVtcy1hc3NldHMvdHZzZWFzb24vNzhlYjU1ODQtOGQxNi00YjhmLTg5NDYtMjNiNTI4ODM2Yjc3LmpwZw==',
+            },{
             id: 'movie2',
+            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie1.jpg',
+            },{
+            id: 'movie3',
             poster: 'https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQePzTRUAxVRh2HMZ_e-VveoJ8aiePGkgnvruipwpBy0LUhMQhKHBDVrjXbcdsSeVcQHqtNCkJcPCAZMpqHoVmxItrAAILt7xtq25abqmWZrTY7vFQEiDK-rjaq4t63GdPEb_TOFpLDwLPeW_PkLhaGx2.jpg?r=93e',
         }, {
-            id: 'movie3',
+            id: 'movie4',
             poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie3.jpg',
         }, {
-            id: 'movie4',
+            id: 'movie5',
             poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie4.jpg',
         }, {
-            id: 'movie5',
+            id: 'movie6',
             poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie2.jpg',
         }]
     }, {
@@ -23,76 +26,76 @@ export default {
         title: 'Trending Now',
         movies: [{
             id: 'movie1',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie5.jpg',
+            poster: 'https://occ-0-2794-2218.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABWHPJmSPglORDqeWm8XuoTQUQocrc7_1Jypr7z_OYddyLcWfXIeq0nE_YUaDT3bWelZQFLn4SXio57FW5bRrqqN9lAOJWRrTmoJUEtuBRyeAeAyQcyvus-rFTQP-Ag.jpg',
         }, {
             id: 'movie2',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie4.jpg',
+            poster: 'https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQePzTRUAxVRh2HMZ_e-VveoJ8aiePGkgnvruipwpBy0LUhMQhKHBDVrjXbcdsSeVcQHqtNCkJcPCAZMpqHoVmxItrAAILt7xtq25abqmWZrTY7vFQEiDK-rjaq4t63GdPEb_TOFpLDwLPeW_PkLhaGx2.jpg?r=93e',
         }, {
             id: 'movie3',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie3.jpg',
+            poster: 'https://occ-0-1723-1722.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABRZ8xR9gq4hRNXY3V64-aTi68bgxzy9Gk7T41wkdVJ64HA-AmGkFjtbRLNUwZXimLFdTN7ab4ZVnwOtxuvfXC0LHfrR_jOpdwRcaDlaY2eSqCrpbur27qlc_SZlNpQ.jpg',
         }, {
             id: 'movie4',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie2.jpg',
+            poster: 'https://m.media-amazon.com/images/M/MV5BOTVjMWY2NzAtODg3My00NTJmLTk2MmEtYmQ1MzAxM2U3MTYwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg',
         }, {
             id: 'movie5',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie1.jpg',
+            poster: 'https://www.nzherald.co.nz/resizer/c7i-kBlByrSJsWmTsuGq3lnQO0M=/576x870/smart/filters:quality(70)/cloudfront-ap-southeast-2.images.arcpublishing.com/nzme/HV7NVI24VZAUHC4IJ66MSCJNQI.JPG',
         }]
     }, {
         id: 'category3',
-        title: 'Latin American Movies & TV',
+        title: 'Movies Produced by Woman',
         movies: [{
             id: 'movie1',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie3.jpg',
+            poster: 'https://static2.showtimes.com/poster/660x980/shes-funny-that-way-5765.jpg',
         }, {
             id: 'movie2',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie2.jpg',
+            poster: 'https://m.media-amazon.com/images/M/MV5BMjM1NjM4NTA1NV5BMl5BanBnXkFtZTgwNTI5MDMxNjM@._V1_.jpg',
         }, {
             id: 'movie3',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie4.jpg',
+            poster: 'https://occ-0-116-114.1.nflxso.net/art/2aa2f/fb9a866f34e5362923101d247827f0f801d2aa2f.jpg',
         }, {
             id: 'movie4',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie5.jpg',
+            poster: 'https://m.media-amazon.com/images/M/MV5BNzE3YTY3NjUtNWY0YS00NmVkLThhYmEtYWQxMjFhN2UwN2IxXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg',
         }, {
             id: 'movie5',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie1.jpg',
+            poster: 'https://itswynnesworld.files.wordpress.com/2018/06/set-it-up-poster.jpg?w=528&h=740',
         }]
     }, {
         id: 'category4',
-        title: 'Comedies',
+        title: 'Documentaries',
         movies: [{
             id: 'movie1',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie5.jpg',
+            poster: 'https://assets.gadgets360cdn.com/pricee/assets/product/202201/Neymar-The-Perfect-Chaos-250_1642533083.jpg',
         }, {
             id: 'movie2',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie1.jpg',
+            poster: 'https://www.digitaltrends.com/wp-content/uploads/2022/06/zbA8GgIBRsVoAO1uEvS6k2rqVAV.jpg?p=1#038;p=1.jpg',
         }, {
             id: 'movie3',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie4.jpg',
+            poster: 'https://www.digitaltrends.com/wp-content/uploads/2022/11/uRdZwrdUisIVux3qmW53P4EDynC.jpg?p=1#038;p=1.jpg',
         }, {
             id: 'movie4',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie5.jpg',
+            poster: 'https://www.digitaltrends.com/wp-content/uploads/2022/06/z1HiEB1UEfdA5fPkngekjQ2vlCC.jpg?p=1#038;p=1.jpg',
         }, {
             id: 'movie5',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie1.jpg',
+            poster: 'https://www.digitaltrends.com/wp-content/uploads/2022/04/3ny9qqgim2m6lgfldaxujtotmbo.jpg?p=1#038;p=1.jpg',
         }]
     }, {
         id: 'category5',
         title: 'Top 10',
         movies: [{
             id: 'movie1',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie4.jpg',
+            poster: 'https://dnm.nflximg.net/api/v6/WNk1mr9x_Cd_2itp6pUM7-lXMJg/AAAABV0XjocMKdlSnEKYsa_hnqk8_1evPkaxQggKvg-qI-sXwbEj6rJzmtP207xSV3bHl0OSbD7QytyhuGAPESu8U4OcC2uDh7_gX5-4E9Cra5NduY2QAhF202CwJmwmvDVQ_MxruQ.jpg?r=f49',
         }, {
             id: 'movie2',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie3.jpg',
+            poster: 'https://www.showbizjunkies.com/wp-content/uploads/2022/09/white-noise-adam-driver-poster.jpg',
         }, {
             id: 'movie3',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie4.jpg',
+            poster: 'https://assets.nflxext.com/us/boxshots/hd1080/80016589.jpg',
         }, {
             id: 'movie4',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie5.jpg',
+            poster: 'https://occ-0-869-586.1.nflxso.net/dnm/api/v6/WNk1mr9x_Cd_2itp6pUM7-lXMJg/AAAABej_GPYjRZOcZuwuzCMhOSKl6j4NAhu3aT7DXiZ8UEGcCoxkzHlTMGFPSpx65qAaN35v9aHqFlXcItjfVeuSz-uvCa4O71R22hwp.jpg',
         }, {
             id: 'movie5',
-            poster: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/netflix/movie1.jpg',
+            poster: 'https://theartsshelf.com/wp-content/uploads/2022/10/Matilda-Main-Poster-1-Sheet-scaled.jpg',
         }]
     }]
 }
